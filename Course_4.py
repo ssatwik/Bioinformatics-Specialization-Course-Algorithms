33ebd1b2747fbfbc7c6d601f96daf03ab16d7878
Nimmagadda Satwik <169017804+ssatwik@users.noreply.github.com>
2024-12-18 23:27:11 +0530

    Update Course_4.py

--- Course_4.py
+++ Course_4.py
@@ -399,3 +399,231 @@ def large_parsimony(lis):
                 new_output=n2_output.copy()
     return n2_lis
 
+
+amino_acid_mass = {'G': 57, 'A': 71, 'S': 87, 'P': 97, 'V': 99, 'T': 101, 'C': 103, 'I': 113, 'L': 113, 'N': 114, 'D': 115, 'K': 128, 'Q': 128, 'E': 129, 'M': 131, 'H': 137, 'F': 147, 'R': 156, 'Y': 163, 'W': 186}
+
+amino_weights = {57,71,87,97,99,101,103,113,114,115,128,129,131,137,147,156,163,186}
+
+rm = {
+    57: 'G', 71: 'A', 87: 'S', 97: 'P', 99: 'V', 101: 'T', 103: 'C', 113: ['L','I'], 114: 'N', 115: 'D', 128: ['K','Q'], 129: 'E', 131: 'M', 137: 'H', 147: 'F', 156: 'R', 163: 'Y', 186: 'W'
+}
+imaginary_amino_acid_by_mass = {4:"X", 5:"Z", 57:"G", 71:"A", 87:"S", 97:"P", 99:"V", 101:"T", 103:"C", 113:"L", 114:"N", 115:"D", 128:"Q", 129:"E", 131:"M", 137:"H", 147:"F", 156:"R", 163:"Y", 186:"W"}
+
+imaginary_mass_by_amino_acid = {"X":4, "Z":5, "G":57, "A":71, "S":87, "P":97, "V":99, "T":101, "C":103, "L":113, "I":113, "N":114, "D":115, "Q":128, "K":128, "E":129, "M":131, "H":137, "F":147, "R":156, "Y":163, "W":186, "I":113, "K":128}
+
+def spectrum_dfs_path(g,x,y,dp):
+    if dp[x]!=['lol']: return dp[x]
+    dp[x]=[]
+    if x not in g: return []
+    for (c,a) in g[x]:
+        l=spectrum_dfs_path(g,c,y,dp)
+        for s in l:
+            s=a+s
+            dp[x].append(s)
+    return dp[x]
+
+def linear_prefix_suffix_spectrum(s):
+    ans=[0]
+    n=len(s)
+    for i in range(n):
+        p=ans[-1]
+        ans.append(p+amino_acid_mass[s[i]])
+    for i in range(n-1):
+        p=ans[-1]
+        ans.append(p-amino_acid_mass[s[i]])
+    ans.sort()
+    return ans
+
+def decoding_ideal_spectrum(l):
+    g={}
+    s=l
+    for x in s:
+        for w in amino_weights:
+            if x+w in s:
+                if isinstance(rm[w],str):
+                    if x not in g: g[x]=[]
+                    g[x].append((x+w,rm[w]))
+                else:
+                    if x not in g: g[x]=[]
+                    g[x].append((x+w,rm[w][0]))
+                    # g[x].append((x+w,rm[w][1]))
+    dp={}
+    for k in g:
+        dp[k]=['lol']
+    dp[s[-1]]=['']
+    ll = spectrum_dfs_path(g,0,s[-1],dp)
+    ans=[]
+    for ss in ll:
+        if linear_prefix_suffix_spectrum(ss) == s: ans.append(ss)
+    return ans
+
+def peptide_to_peptide_vector(s):
+    n=len(s)
+    l=[]
+    temp=0
+    for x in s:
+        x=imaginary_mass_by_amino_acid[x]
+        temp+=x
+        l.append(temp)
+    ans=[0 for i in range(l[-1])]
+    for x in l: ans[x-1]=1
+    return ans
+
+def peptide_vector_to_peptide(l):
+    m=[]
+    for i in range(len(l)):
+        if l[i]==1: m.append(i+1)
+    s=imaginary_amino_acid_by_mass[m[0]]
+    for i in range(1,len(m)):
+        s+=imaginary_amino_acid_by_mass[m[i]-m[i-1]]
+    return s
+
+def peptide_sequencing_dfs(g,x,dp,sv):
+    n=len(g)
+    if x>=n: return []
+    if dp[x]!=['lol',0]: return dp[x]
+    scm=0
+    f=0
+    dps=''
+    for (c,a) in g[x]:
+        l=peptide_sequencing_dfs(g,c,dp,sv)
+        if l==[]: continue
+        s=l[0]
+        sc=l[1]
+        if sc+sv[c]>scm or f==0:
+            f=1
+            dps=a+s
+            scm=sc+sv[c]
+    if f==1: dp[x]=[dps,scm]
+    else: dp[x]=[]
+    return dp[x]
+    
+def peptide_sequencing(sv):
+    sv.insert(0,0)
+    n=len(sv)
+    g=[[] for i in range(n)]
+    for i in range(n):
+        for w,a in imaginary_amino_acid_by_mass.items():
+            if i+w<n:
+                g[i].append((i+w,a))
+    dp=[['lol',0] for i in range(n)]
+    dp[n-1]=['',0]
+    ll = peptide_sequencing_dfs(g,0,dp,sv)
+    return ll
+
+def peptide_score(pv,sv):
+    n=len(pv)
+    ans=0
+    for i in range(n):
+        ans+=pv[i]*sv[i]
+    return ans
+
+def best_peptide_in_proteome(p,sv):
+    n=len(p)
+    w=len(sv)
+    l=[]
+    for i in range(n):
+        temp=imaginary_mass_by_amino_acid[p[i]]
+        j=i
+        for jj in range(i+1,n):
+            if temp>=w: break
+            temp+=imaginary_mass_by_amino_acid[p[jj]]
+            j=jj
+        if temp!=w: continue
+        l.append(p[i:j+1])
+    if len(l)==0: return '',0
+    ll=[]
+    for i in range(len(l)):
+        ll.append(peptide_score(peptide_to_peptide_vector(l[i]),sv))
+    ma=max(ll)
+    ans=[]
+    for i in range(len(l)):
+        if ll[i]==ma:
+            ans.append(l[i])
+    # return ans
+    return ans[0],ma
+
+def psm_search(p,l,th):
+    ans=set()
+    for sv in l:
+        x,y=best_peptide_in_proteome(p,sv)
+        if x=='': continue
+        if y>=th: ans.add(x)
+    return ans
+
+def spectral_dictionary_size(v,th,ma):
+    v.insert(0,0)
+    shift=0
+    p=0
+    for x in v:
+        if x<0: shift-=x
+        else: p+=x
+    dp=[[0 for i in range(p+shift+1)] for j in range(len(v))]
+    weights = [57,71,87,97,99,101,103,113,113,114,115,128,128,129,131,137,147,156,163,186]
+    dp[0][shift]=1
+    for i in range(1,len(v)):
+        for s in range(p+shift+1):
+            for w in weights:
+                # if i-w<0 or s-v[i]<0 or s-v[i]>p+shift: continue
+                if i-w<0 or s-v[i]<shift or s-v[i]>ma+shift: continue
+                dp[i][s]+=dp[i-w][s-v[i]]
+    ans=0
+    for i in range(shift+th,shift+ma+1): ans+=dp[len(v)-1][i]
+    return ans
+
+def spectral_dictionary_probability(v,th,ma):
+    v.insert(0,0)
+    shift=0
+    p=0
+    for x in v:
+        if x<0: shift-=x
+        else: p+=x
+    dp=[[0 for i in range(p+shift+1)] for j in range(len(v))]
+    weights = [57,71,87,97,99,101,103,113,113,114,115,128,128,129,131,137,147,156,163,186]
+    dp[0][shift]=1
+    for i in range(1,len(v)):
+        for s in range(p+shift+1):
+            for w in weights:
+                if i-w<0 or s-v[i]<0 or s-v[i]>p+shift: continue
+                # if i-w<0 or s-v[i]<shift or s-v[i]>ma+shift: continue
+                dp[i][s]+=dp[i-w][s-v[i]]*(0.05)
+    ans=0
+    for i in range(shift+th,shift+ma+1): ans+=dp[len(v)-1][i]
+    return ans
+
+def spectral_alignment_with_k_mutations(s,v,k):
+    p=[]
+    for a in s: p.append(imaginary_mass_by_amino_acid[a])
+    v.insert(0,0)
+    n=len(v)
+    dp=[[('lol',0,0) for i in range(k+1)] for j in range(n)]
+    dp[n-1][0]=('',0,len(p))
+    for ia in range(len(p)-1,-1,-1):
+        w=p[ia]
+        a=s[ia]
+        for i in range(n):
+            for j in range(k+1):
+                if dp[i][j][0]=='lol' or dp[i][j][2]!=ia+1: continue
+                ss=a+dp[i][j][0]
+                sc=v[i]+dp[i][j][1]
+                if i-w>=0:  
+                    if sc>dp[i-w][j][1] or dp[i-w][j][0]=='lol':
+                        dp[i-w][j]=(ss,sc,ia)
+                if j==k: continue
+                for q in range(n):
+                    if q==i-w: continue
+                    if sc>dp[q][j+1][1] or dp[q][j+1][0]=='lol':
+                        x = i-w-q
+                        if x<0: ss=f"{a}({x}){dp[i][j][0]}"
+                        else: ss=f"{a}(+{x}){dp[i][j][0]}"
+                        dp[q][j+1]=(ss,sc,ia)
+    ans=''
+    max=0
+    f=0
+    for j in range(k+1):
+        if dp[0][j][0]=='lol': continue
+        if max<dp[0][j][1] or f==0:
+            f=1
+            max=dp[0][j][1]
+            ans=dp[0][j][0]
+    return ans
